345452c4a5579c8b54f0acedbcd2edd4bf98cc40
unknown <thewhiterabbit1994@outlook.com>
2022-02-23 10:55:45 +0330

    fixed the auth bug

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import RequiredAuth from "./RequiredAuth";
 function App() {
   return (
     <div className="App">
-      <div style={{display:'flex',justifyContent:'space-between',marginBottom:'20px'}}>  
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>  
         <Link to="/">Home</Link>
         <Link to="/pricing">Pricing</Link>
       </div>
--- src/RequiredAuth.js
+++ src/RequiredAuth.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import {Navigate, useLocation} from 'react-router-dom';
-import {useAuth} from './Context';
+import React, {useContext} from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+import {authContext} from './Context'
+// import {useAuth} from './Context';
 
 function RequiredAuth({children}) {
-  const {authed} = useAuth();
+  const {authed} = useContext(authContext);
   const Location = useLocation();
   return authed == true ? children : <Navigate to="/login" replace state={{path: Location.pathname}}/> ;
 }
